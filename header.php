5a854bfd542c2997a82ec412e5c9ee44bb5ee9a8
SunixHigh <suni@host7-55.cust.ktab.fi>
2024-04-23 09:21:03 +0300

    Add some more files, and now you must be able to delete and edit books on the page

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 include "Includes/class.user.php";
 include "Includes/config.php";
-include "functions.php";
+include "Includes/functions.php";
 
 if (isset($_POST['logout'])) {
     if ($user->Logout()) {
